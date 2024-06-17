6b6e2643f719718bfd887d5f77ff5a2c5cac3bd3
chillrod <chillrod77@gmail.com>
2024-06-16 21:08:39 -0300

    fix: type changes

--- src/services/layers/layers-image.ts
+++ src/services/layers/layers-image.ts
@@ -2,7 +2,7 @@ import type { ExtendedFabricObject } from "@/types/fabric";
 
 export const createImageModel = (item: ExtendedFabricObject) => {
   const imageModel = {
-    image: item?.getSrc(),
+    image: item.getSrc ? item.getSrc() : "",
   };
 
   return imageModel;
