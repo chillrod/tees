c81c9efd1adf2d9d1e1047be2ecf1b032fa228b9
chillrod <chillrod77@gmail.com>
2024-09-04 17:53:20 -0300

    fix: move firebase data to .env

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,16 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCsf2NntJwZ452R2dNbvTrizhIXAUH7UUc",
-  authDomain: "studioestamparcanvas.firebaseapp.com",
-  projectId: "studioestamparcanvas",
-  storageBucket: "studioestamparcanvas.appspot.com",
-  messagingSenderId: "493173877593",
-  appId: "1:493173877593:web:2b33b34644acc60126fa48",
-  measurementId: "G-6DF9QM3Z69"
+  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
+  measurementId: import.meta.env.PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
 
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
